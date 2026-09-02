de5839b980eae9bfc94ee825baa056be4964e8d8
Sarkred76 <148964265+Sarkred76@users.noreply.github.com>
2026-09-03 01:13:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5873,8 +5873,8 @@ async def decline_clan_invite_callback(update: Update, context: ContextTypes.DEF
                 text=(
                     f"❌ Игрок {html.escape(user_data.get('first_name', 'Игрок'))} "
                     f"отклонил приглашение в клан <b>{html.escape(clan_name)}</b>.",
-                    parse_mode="HTML"
                 )
+                parse_mode="HTML"
             )
         except:
             pass
