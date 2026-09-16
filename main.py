55237a109b0f56093b47fd7824a6811b98b56389
Sarkred76 <148964265+Sarkred76@users.noreply.github.com>
2026-09-16 22:44:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10684,8 +10684,7 @@ async def event_menu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None
                 await update.message.reply_text(
                     "🎩 <b>Ивент</b>\n\n"
                     "🔒 <b>Ивент завершён!</b>\n\n"
-                    "Вы уже прошли допрос и обменяли карты Injustice.\n"
-                    "Ожидайте новых расследований...",
+                    "Вы уже прошли допрос и обменяли карты Injustice.\n",
                     reply_markup=ReplyKeyboardMarkup(keyboard, resize_keyboard=True),
                     parse_mode="HTML"
                 )
